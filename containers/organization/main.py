ac21427cecd9ad06f66bc367b26969912d81f108
nibaldo <nibaldo.donoso@cosmotech.com>
2024-12-03 12:55:38 +0100

    remove id field

--- containers/organization/main.py
+++ containers/organization/main.py
@@ -118,7 +118,6 @@ def main():
                 res_ = None
                 if not resource_data.get("id"):
                     res_ = create(data=resource_data)
-                    custom_resource["spec"]["status"] = "CREATED"
                     custom_resource["spec"]["id"] = res_.get("id")
                 try:
                     api_instance.patch_namespaced_custom_object(
